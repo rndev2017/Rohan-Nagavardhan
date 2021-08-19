14da5df2f55777ecb3ffcebed5a642eb34680944
Rohan Nagavardhan <rohans415@gmail.com>
2021-08-19 17:17:00 -0400

    fixed modal issue

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import TwitterIcon from '../components/TwitterIcon'
 
 export default function Site() {
   return (
-    <div>
+    <div className="overflow-hidden">
       <Head>
         <title>Rohan Nagavardhan</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
