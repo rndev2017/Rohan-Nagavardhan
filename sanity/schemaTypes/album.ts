96d4c8c244b6412c6e84711688f37800d75a3cc7
Rohan Nagavardhan <rohans415@gmail.com>
2025-03-15 12:13:03 -0700

    add album type

---
+++ sanity/schemaTypes/album.ts
@@ -0,0 +1,40 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'album',
+  title: 'Album',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'description',
+      title: 'description',
+      type: 'array',
+      of: [{'type': 'block'}]
+    }),
+
+    defineField({
+      title: 'Started at',
+      name: 'startedAt',
+      type: 'date',
+      options: {
+        dateFormat: 'MMM Do, YY',
+      }
+    }),
+
+    defineField({
+      title: 'Ended at',
+      name: 'endedAt',
+      type: 'date',
+      options: {
+        dateFormat: 'MMM Do, YY',
+      }
+    })
+  ]
+
+})
